e2c4c9d33503b12cd7855efa19ab93dedee4f2af
Akshay-2406 <akshay.konkepudi@gmail.com>
2026-08-21 22:15:16 +0530

    Update password_lock.v

--- rtl/password_lock.v
+++ rtl/password_lock.v
@@ -1,1 +1,29 @@
+module password_lock(
+    input [3:0] key,
+    input key_strobe,
+    output reg locked_led,
+    output reg unlock_led,
+    output reg error_led
+);
 
+    parameter [3:0] PASSWORD = 4'b1010;
+
+    always @(*) begin
+        locked_led = 1;
+        unlock_led = 0;
+        error_led = 0;
+
+        if (key_strobe) begin
+            if (key == PASSWORD) begin
+                unlock_led = 1;
+                locked_led = 0;
+                error_led = 0;
+            end else begin
+                error_led = 1;
+                locked_led = 1;
+                unlock_led = 0;
+            end
+        end
+    end
+
+endmodule
